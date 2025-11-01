c73f29dc3ffd0ca6d365666ffbddd4abd41bc2c7
Ladan <119695832+ladan-hazrati-web@users.noreply.github.com>
2025-11-01 10:46:05 +0330

    Create AuthCallback.jsx

---
+++ src/components/privateRoute/AuthCallback.jsx
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { supabase } from "../../supabase/supabaseClient";
+import { useDispatch } from "react-redux";
+import { setAuthStatus, setUser, setUserId } from "../slices/authSlice";
+
+export default function AuthCallback() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const { data: listener } = supabase.auth.onAuthStateChange(
+      async (event, session) => {
+        if (event === "SIGNED_IN" && session) {
+          dispatch(setAuthStatus(true));
+          dispatch(setUser(session));
+          dispatch(setUserId(session.user.id));
+          localStorage.setItem("data", JSON.stringify(session));
+          navigate("/", { replace: true });
+        }
+      }
+    );
+
+    return () => listener.subscription.unsubscribe();
+  }, [dispatch, navigate]);
+
+  return <p>در حال ورود...</p>;
+}
